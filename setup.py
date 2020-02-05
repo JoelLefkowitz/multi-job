351835e590eb70db72e9f4664634e7226933645c
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-05 16:55:34 +0000

    WIP Modify setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 s = setup(
     name="multi-job",
-    version="0.8.11",
+    version="0.9.0",
     license="MIT",
     description="Job runner for multifaceted projects",
     long_description=long_description,
