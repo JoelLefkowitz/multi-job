04ffb794c609265568b28c20298582debe832d78
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-05 17:10:23 +0000

    Bump version: 0.9.3 → 0.9.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 s = setup(
     name="multi-job",
-    version="0.9.3",
+    version="0.9.4",
     license="MIT",
     description="Job runner for multifaceted projects",
     long_description=long_description,
