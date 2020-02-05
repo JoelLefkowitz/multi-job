32602f45adcfb564463b6913d65b2bae0ef70de5
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-05 16:38:55 +0000

    Modify bumpversion config 2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 s = setup(
     name="multi-job",
-    version="0.8.11",
+    version="0.8.10",
     license="MIT",
     description="Job runner for multifaceted projects",
     long_description=long_description,
