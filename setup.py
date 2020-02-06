a0d011f5465073bfad3c1a86fdaeec339e546db6
JoelLefkowitz <joellefkowitz@hotmail.com>
2020-02-06 19:34:43 +0000

    Bump version: 0.9.14 → 0.9.15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ print(find_packages())
 
 s = setup(
     name="multi-job",
-    version="0.9.14",
+    version="0.9.15",
     license="MIT",
     description="Job runner for multifaceted projects",
     long_description=long_description,
